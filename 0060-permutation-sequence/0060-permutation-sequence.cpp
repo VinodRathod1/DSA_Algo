9a190e1baa4413c66e5f7a4acf32d63f519985f4
VinodRathod1 <121213916+VinodRathod1@users.noreply.github.com>
2025-10-22 15:42:50 +0530

    Time: 543 ms (5.86%), Space: 8 MB (84.17%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
+    int cnt=0;
+    string ans="";
+    void rec(vector<int>&nums,vector<bool>&used,string& curr,int k){
+        if(curr.size()==nums.size()){
+            cnt++;
+            if(cnt==k)ans=curr;
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(!used[i]){
+                used[i]=true;
+                curr.push_back(nums[i]+'0');
+                rec(nums,used,curr,k);
+                if(!ans.empty())return ;
+                curr.pop_back();
+                used[i]=false;
+            }
+        }
+    }
     string getPermutation(int n, int k) {
         vector<int>nums;
-        int fact=1;
-        for(int i=1;i<=n;i++){
-            fact*=i;
-            nums.push_back(i);
-        }
-        k--;
-        string ans="";
-        for(int i=n;i>=1;i--){
-            fact/=i;
-            int index=k/fact;
-            ans+=to_string(nums[index]);
-            nums.erase(nums.begin()+index);
-            k%=fact;
-        }
+        for(int i=1;i<=n;i++)nums.push_back(i);
+        vector<bool>used(n,false);
+        string curr="";
+        rec(nums,used,curr,k);
         return ans;
     }
 };
