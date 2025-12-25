9580dda06b08367bfc1a59a08f503d7407a473c0
VinodRathod1 <121213916+VinodRathod1@users.noreply.github.com>
2025-12-25 12:53:36 +0530

    Time: 2 ms (81.79%), Space: 13.1 MB (82.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string>res;
-        
-        rec(2*n,res,"",0,0);
+        string curr="";
+        rec(2*n,curr,res,0,0);
         return res;
     }
-    void rec(int len,vector<string>&res,string curr,int open,int close){
-        if(open>(len/2) || close>(len/2) || close>open)return;
+    void rec(int len,string& curr,vector<string>&res,int open,int close){
+      
         if(curr.length()==len){
             res.push_back(curr);
             return;
         }
-        
-        rec(len,res,curr+"(",open+1,close);
-        rec(len,res,curr+")",open,close+1);
+        if(open<len/2){
+            curr.push_back('(');
+            rec(len,curr,res,open+1,close);
+            curr.pop_back();
+        }
+        if(close<open){
+            curr.push_back(')');
+            rec(len,curr,res,open,close+1);
+            curr.pop_back();
+        }
     }
 };
