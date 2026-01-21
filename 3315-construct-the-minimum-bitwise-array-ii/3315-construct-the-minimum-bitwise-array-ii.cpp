8609444c91043986d78fa7fe41a92fc98c55ad40
VinodRathod1 <121213916+VinodRathod1@users.noreply.github.com>
2026-01-21 10:13:09 +0530

    Time: 0 ms (100%), Space: 28.2 MB (83.5%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-           vector<int> ans;
-
-        for (int x : nums) {
-            if (x == 2) {
-                ans.push_back(-1);
-                continue;
-            }
-
-            int cnt = 0;
-            int temp = x;
-
-            // count trailing 1s
-            while (temp & 1) {
+        vector<int>ans;
+        for(int x:nums){
+            int cnt=0;
+            int temp=x;
+            while(temp&1){
                 cnt++;
-                temp >>= 1;
+                temp>>=1;
             }
-
-            ans.push_back(x - (1 << (cnt - 1)));
+            if(cnt==0){
+                ans.push_back(-1);
+            }else{
+                ans.push_back(x-(1<<(cnt-1)));
+            }
+            
         }
-
         return ans;
     }
 };
