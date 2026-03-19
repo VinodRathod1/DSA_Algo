2ebcda49b5136b6210f67756a8e3f09df6058deb
VinodRathod1 <121213916+VinodRathod1@users.noreply.github.com>
2026-03-19 23:54:34 +0530

    Time: 0 ms (100%), Space: 13.2 MB (63.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def twoSum(self, nums, target):
-       mpp = {}
+        mpp={}
+        for i in range(len(nums)):
+            c=target-nums[i]
+            if c in mpp:
+                return [mpp[c],i]
+            else:
+                mpp[nums[i]]=i
 
-       for i in range(len(nums)):
-           c=target-nums[i]
-           if c in mpp:
-               return [mpp[c],i]
-           else:
-               mpp[nums[i]]=i
-            
-       return []
+        return []
+      
 
        
